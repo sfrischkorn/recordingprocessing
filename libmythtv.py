15952044f7effb5ff0863ddda475db51a6fcaf32
Shane Frischkorn <shanef@gmail.com>
2013-07-05 14:48:21 +1000

    Added mythtv logic

--- libmythtv.py
+++ libmythtv.py
@@ -5,3 +5,17 @@ Created on Fri Jul  5 14:10:47 2013
 @author: shanef
 """
 
+import MySQLdb as mdb
+from libtvdatasource import TVShow
+
+class MythTV:
+    def RetrieveEpisodeData(serverAddress, user, password, database, inputFile):
+        con = mdb.connect(serverAddress, user, password, database)
+        
+        with con:
+            cur = con.cursor(mdb.cursors.DictCursor)
+            cur.execute("select episode, season, title, subtitle, description from mythconverg.recorded where basename = '{0}'".format(inputFile))
+            result = cur.fetchone()
+            
+            return TVShow(result['episode'], result['season'], result['title'], result['subtitle'], result['description'])
+      
